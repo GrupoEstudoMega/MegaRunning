a4df4abc1f685226e53223891a9fbc9a960f728c
gallasch <christian.gallasch@gmail.com>
2014-08-20 19:30:02 -0300

    Sessao 4

--- main.go
+++ main.go
@@ -9,8 +9,9 @@ import (
 func main() {
 
 	http.HandleFunc("/prova", prova.Handler)
+	fs := http.Dir("public/")
+	http.Handle("/", http.FileServer(fs))
 	fmt.Println("Rodando na porta 8080")
 	http.ListenAndServe(":8080", nil)
 
 }
-
--- prova/prova.go
+++ prova/prova.go
@@ -1,36 +1,48 @@
 package prova
 
-import(
-"net/http"
-"gopkg.in/mgo.v2"
-"gopkg.in/mgo.v2/bson"
-"encoding/json"
-"../conexao"
-"fmt"
+import (
+	"../conexao"
+	"encoding/json"
+	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"net/http"
 )
 
 var collection *mgo.Collection = conexao.GetDb().C("Prova")
 
-func Handler(response http.ResponseWriter , request *http.Request){
+func Handler(response http.ResponseWriter, request *http.Request) {
 	fmt.Println(request.Method)
 	switch request.Method {
-		//case "GET" : 
+	case "GET":
+		provas := Provas()
+		fmt.Println(provas)
+		js, _ := json.MarshalIndent(provas, " ", "   ")
+		response.Write(js)
+		//resp := json.NewEncoder(response)
+		//resp.Encode(provas)
 
-		case "POST" :
-			var prova Prova 
-			json.NewDecoder(request.Body).Decode(&prova)
-			err := Insere(&prova)
-			fmt.Println(err)
+	case "POST":
+		var prova Prova
+		json.NewDecoder(request.Body).Decode(&prova)
+		err := Insere(&prova)
+		fmt.Println(err)
 
 		//case "DELETE" :
 
-	}	
+	}
 }
 
-func Insere(prova *Prova) error{
+func Provas() []Prova {
+	var provas []Prova
+	collection.Find(nil).All(&provas)
+	return provas
+}
 
-    prova.Id = bson.NewObjectId()
-    fmt.Println(prova)
+func Insere(prova *Prova) error {
+
+	prova.Id = bson.NewObjectId()
+	fmt.Println(prova)
 	return collection.Insert(prova)
-	
+
 }
